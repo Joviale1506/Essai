8b53d9e3a509c57f722f6f0cd42dd22d3a6a3829
Joviale1506 <155860284+Joviale1506@users.noreply.github.com>
2024-01-07 03:57:07 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+const {google} = require('googleapis');
+
+exports.handler = async function(event, context) {
+    // Récupérer l'email à partir de la requête
+    const email_to_add = event.queryStringParameters.email;
+
+    // Chemin vers le fichier de clé du compte de service
+    const keyFile = 'C:\\Users\\Joviale\\IdeaProjects\\Newsletter\\newsletter-410220-5dc4f904e977.json';
+
+    // Construire le service d'authentification
+    const auth = new google.auth.GoogleAuth({
+        keyFile: keyFile,
+        scopes: ['https://www.googleapis.com/auth/admin.directory.group.member']
+    });
+    const authClient = await auth.getClient();
+    const service = google.admin({version: 'directory_v1', auth: authClient});
+
+    // Ajouter l'email au groupe Google
+    const groupKey = 'newsletter-one-piece-phoenix@newsletter-410220.iam.gserviceaccount.com';
+    const res = await service.members.insert({
+        groupKey: groupKey,
+        requestBody: {
+            email: email_to_add,
+            role: 'MEMBER'
+        }
+    });
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify({message: "Email ajouté avec succès au groupe !"})
+    };
+}
+
